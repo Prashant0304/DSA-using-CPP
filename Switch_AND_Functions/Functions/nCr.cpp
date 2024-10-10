701b392c72a6ac0e48cc5551a8fcc07aeb21b9f9
unknown <prashantpattanashetti5@gmail.com>
2024-10-10 19:14:53 +0530

    Function with return type with return value

--- Switch_AND_Functions/Functions/nCr.cpp
+++ Switch_AND_Functions/Functions/nCr.cpp
@@ -1,29 +1,24 @@
+// Function with return type with return value
 #include<iostream>
 using namespace std;
 
-int factorial(int n)
-{
-    int fact=1;
-    for(int i=1;i<=n;i++)
-    {
-         fact=fact*i;
+int factorial(int n){
+    int ans=1;
+    for(int i=1;i<=n;i++){
+        ans=ans*i;
     }
-    return fact;
+    return ans;
 }
-
-int nCr(int n, int r)
-{
-    int num = factorial(n);
-    int deno = factorial(r)*factorial(n-r);
-    return num/deno;
+int nCr(int n, int r){
+    int num=factorial(n);
+    int den=factorial(r)*factorial(n-r);
+    int nCr=num/den;
+    return nCr;
 }
-
-int main()
-{
+int main(){
     int n,r;
-    cout<<"Enter n value: ";
-    cin>>n;
-    cout<<"Enter r value: ";
-    cin>>r;
-    cout<<"The answer is "<<nCr(n,r);
+    cout<<"Enter n and r value: ";
+    cin>>n>>r;
+    int ans=nCr(n,r);
+    cout<<"Answer of nCr : "<<ans;
 }
