4727b1469e4a6ae2ded13275a21385e2b747b0d5
unknown <prashantpattanashetti5@gmail.com>
2024-10-15 21:48:27 +0530

    swaping using reference of variable

--- Reference/reference.cpp
+++ Reference/reference.cpp
@@ -4,19 +4,14 @@ using namespace std;
 int update1(int& a){
     cout<<a++;
 }
-int update2(int& a){
-    cout<<a++;
-}
-int update3(int& a){
-    cout<<a++;
-}
+
 int main(){
     int n=5;
     cout<<"Update variable: ";
-    update2(n);
+    update1(n);
     cout<<endl<<"Update reference: ";
     update1(n);
     cout<<endl<<"Update reference: ";
-    update3(n);
+    update1(n);
     return 0;
 }
