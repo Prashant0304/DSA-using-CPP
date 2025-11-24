8cf5693837b60f22b5f7498a7e3ef1519e5e323d
Prashant0304 <prashantpattanashetti5@gmail.com>
2025-11-24 12:54:45 +0530

    Updated Bubblesort problem

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,18 +1,18 @@
 class Bubblesort{
-	public static void main(String argas[]){
-		 int a[] ={100,4,6,2,1,76,98};
-		 for(int i=0 ;i < a.length-1 ;i++){
-			 for(int j=0;j<a.length-1-i;j++){
-				 if(a[j] > a[j+1]){
-					 int temp = a[j];
-					 a[j] = a[j+1];
-					 a[j+1] = temp;
-				 }
-			 }
-		 }
-		 System.out.println("Ascending order :");
-		 for(int number : a){
-			 System.out.println(number);
-		 }
+	public static void main(String args[]){
+		int a[] ={23,6,7,8443,3,56,78,99};
+		for(int i=0;i<a.length-1;i++){
+			for(int j=0;j<a.length-1-i;j++){
+				if(a[j]<a[j+1]){
+					int temp = a[j];
+					a[j]= a[j+1];
+					a[j+1] =temp;
+				}
+			}
+		}
+		System.out.println("Ascending Order : ");
+		for(int numbers : a){
+			System.out.println(numbers);
+		}
 	}
 }
