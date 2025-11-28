a575950d41d61e196c89e681f215af4889f7a040
Prashant0304 <prashantpattanashetti5@gmail.com>
2025-11-28 12:42:13 +0530

    Selection sort

--- SelectionsortTrace.java
+++ SelectionsortTrace.java
@@ -1,22 +1,21 @@
-class SelectionsortTrace {
-    public static void main(String args[]) {
-        int a[] = {12, 43, 9, 43, 65, 3, 6, 74};
-        for (int i = 0; i < a.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[j] < a[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            int temp = a[minIndex];
-            a[minIndex] = a[i];
-            a[i] = temp;
-
-            System.out.print("Pass " + (i + 1) + ": ");
-            for (int num : a) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
-    }
+class SelectionsortTrace{
+	public static void main(String args[]){
+		int a[] ={21,32,4,554,656,78,1,98,43,22};
+		for(int i=0;i<a.length-1;i++){
+			int minIndex=i;
+			for(int j= i+1;j<a.length;j++){
+				if(a[j]<a[minIndex]){
+					minIndex = j;
+				}
+			}
+			int temp = a[minIndex];
+			a[minIndex] = a[i];
+			a[i] = temp;
+			System.out.print("Pass "+(i+1)+": ");
+			for(int num : a){
+				System.out.print(num+" ");
+			}
+			System.out.println();
+		}
+	}
 }
