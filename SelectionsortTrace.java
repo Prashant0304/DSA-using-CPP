a5c0e8630fa9479b1b41b046d68cb140ab1dc44b
Prashant0304 <prashantpattanashetti5@gmail.com>
2025-12-01 21:05:54 +0530

    Selection sort problem

--- SelectionsortTrace.java
+++ SelectionsortTrace.java
@@ -2,17 +2,18 @@ class SelectionsortTrace{
 	public static void main(String args[]){
 		int a[] ={21,32,4,554,656,78,1,98,43,22};
 		for(int i=0;i<a.length-1;i++){
-			int minIndex=i;
-			for(int j= i+1;j<a.length;j++){
-				if(a[j]<a[minIndex]){
-					minIndex = j;
-				}
+			int minIndex = i;
+			for(int j=i+1;j<a.length;j++){
+				if(a[j] < a[minIndex]){
+					minIndex=j;
 			}
+		}
 			int temp = a[minIndex];
 			a[minIndex] = a[i];
 			a[i] = temp;
-			System.out.print("Pass "+(i+1)+": ");
-			for(int num : a){
+			
+			System.out.print("Pass"+(i+1)+":");
+			for(int num:a){
 				System.out.print(num+" ");
 			}
 			System.out.println();
