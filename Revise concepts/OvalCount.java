e05adc7b8099a2572178cfad6e97a338c577becc
Prashant0304 <prashantpattanashetti5@gmail.com>
2026-07-11 17:38:51 +0530

    Count number of vowels in given string

---
+++ Revise concepts/OvalCount.java
@@ -0,0 +1,13 @@
+class OvalCount{
+	public static void main(String args[]){
+		String str="GADAG";
+		str=str.toUpperCase();
+		int count=0;
+		for(int i=0;i<str.length();i++){
+			if((str.charAt(i)=='A')||(str.charAt(i)=='E')||(str.charAt(i)=='I')||(str.charAt(i)=='O')||(str.charAt(i)=='U')){
+				count++;
+			}
+		}
+		System.out.println("Vowels "+count);
+	}
+}
