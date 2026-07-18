76287aef6cd3fba87bd17cce667f9864ef58ee4f
Prashant0304 <prashantpattanashetti5@gmail.com>
2026-07-18 08:37:21 +0530

    LinkedList methods add , get , addFirst , addLast  used in this to understand concept of LinkedList

---
+++ Revise concepts/Linkedlist.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+class Linkedlist{
+	public static void main(String args[]){
+		LinkedList<Integer> linkedlist = new LinkedList<>();
+		linkedlist.add(84);
+		linkedlist.add(87);
+		linkedlist.add(65);
+		linkedlist.add(54);
+		
+		for(Integer list:linkedlist){
+			System.out.println("LinkedList values : "+list);
+		}
+		
+		linkedlist.addFirst(100);
+		
+		linkedlist.addLast(500);
+		
+		for(Integer list:linkedlist){
+			System.out.println("updated LinkedList values : "+list);
+		}
+		
+		System.out.println("LinkedList values : "+linkedlist.get(8));//outofcound exception
+		
+	}
+}
